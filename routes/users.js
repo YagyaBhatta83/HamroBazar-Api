26ad541f1128d2e26daa3ff2d8ac99c775a7f281
YagyaBhatta83 <bhattayagya123@gmail.com>
2020-01-05 13:45:54 +0545

    auth file created.

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+const route = express.Router();
+const auth = require('../auth');
